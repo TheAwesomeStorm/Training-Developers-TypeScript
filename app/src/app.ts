b9dfe954bd31fd43cccbffb89d83e446036ce64c
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-02 13:48:19 -0300

    Removed unnecessary import roles button

--- app/src/app.ts
+++ app/src/app.ts
@@ -4,15 +4,13 @@ import { RequestController } from './controllers/request-controller.js'
 
 const controller = new RequestController()
 
-// const page = document.querySelector('html') as HTMLElement
+const pageBody = document.querySelector('body') as HTMLElement
 
 const pageTitle = document.querySelector('title') as HTMLElement
 
 const apiSection = document.querySelector('.api') as HTMLElement
 
 const buttonImportUsers = document.querySelector('.import-users') as HTMLElement
-
-const buttonImportRoles = document.querySelector('.import-roles') as HTMLElement
 
 const buttonReturn = document.querySelector('.return') as HTMLElement
 
@@ -21,33 +19,31 @@ const buttonHome = document.querySelector('.home') as HTMLElement
 buttonImportUsers.addEventListener('click', () => {
   controller.importUsers()
   buttonImportUsers.classList.add('hide')
-  buttonImportRoles.classList.remove('hide')
   pageTitle.innerHTML = 'Users | SPA'
-})
-
-buttonImportRoles.addEventListener('click', () => {
-  controller.importRoles('Astolfo')
-  buttonImportRoles.classList.add('hide')
-  buttonReturn.classList.remove('hide')
-  pageTitle.innerHTML = 'Roles | SPA'
 })
 
 buttonReturn.addEventListener('click', () => {
   buttonReturn.classList.add('hide')
-  buttonImportRoles.classList.remove('hide')
   controller.importUsers()
   pageTitle.innerHTML = 'Users | SPA'
 })
 
 buttonHome.addEventListener('click', () => {
   buttonImportUsers.classList.remove('hide')
-  buttonImportRoles.classList.add('hide')
   buttonReturn.classList.add('hide')
   apiSection.innerHTML = ''
   pageTitle.innerHTML = 'Home | SPA'
 })
 
-// page.addEventListener('click', function (event: Event) {
+pageBody.addEventListener('click', function (event: Event) {
+  const target = event.target as HTMLElement
+  if (target.classList.contains('user-name')) {
+    controller.importRoles(target.innerText)
+    buttonReturn.classList.remove('hide')
+  }
+})
+
+// pageBody.addEventListener('click', function (event: Event) {
 //   const target = event.target as Element
 //   const parentTag = target.parentNode as HTMLElement
 //   const tableRows = document.querySelectorAll('tr')
